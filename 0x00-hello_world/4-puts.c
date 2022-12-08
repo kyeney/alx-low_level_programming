61b2cbba8b20fe07ec930fc49f7d9da72c9f5be3
kyeney <irungukenneth69@gmail.com>
2022-12-08 14:39:27 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
-
 /**
  * Main - Entry Point
- *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
-
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
