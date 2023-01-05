824f79fe424b4c128872cfbbbd69f17c675a9778
kyeney <irungukenneth69@gmail.com>
2023-01-05 06:09:16 -0800

    fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,13 +10,13 @@
 
 int _pow_recursion(int x, int y)
 {
-	int result= x;
+	int result = x;
 
 	if (y < 0)
 		return (-1);
 	else if (y == 0)
-		return(1);
-	result *= _pow_recursion(x,y - 1);
+		return (1);
+	result *= _pow_recursion(x, y - 1);
 
 	return (result);
 }
